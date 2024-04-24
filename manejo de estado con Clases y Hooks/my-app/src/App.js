05655759a4b70b9b85fd941c032dfa05345f726c
Jose <josemchapela@gmail.com>
2024-04-24 20:06:28 +0200

    Practica tipos de estado

---
+++ manejo de estado con Clases y Hooks/my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import FullName from './components/FullName';
+
+function App() {
+  return (
+    <div className="App">
+      <FullName name={'Open'} lastname={'Webinars'}></FullName>
+    </div>
+  );
+}
+
+export default App;
